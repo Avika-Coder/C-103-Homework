d5080a0748d1f0a40ecc765ce1f0937665071da5
Avika-Coder <76557800+Avika-Coder@users.noreply.github.com>
2022-03-28 23:39:10 +0530

    Update Data_Visulization-Homework.py

--- Data_Visulization-Homework.py
+++ Data_Visulization-Homework.py
@@ -4,5 +4,5 @@ from google.colab import files
 data_to_load=files.upload()
 
 df=pd.read_csv("data.csv")
-fig=px.bar(df,x='Country',y='InternetUsers')
+fig = px.scatter(df, x="date",y="cases",color="country")
 fig.show()
